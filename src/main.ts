0e2cd8e8b56acc7b90ad6f0d84ca7d5ee050a109
danieltanych <danieltanych@gmail.com>
2025-12-30 13:35:39 +0200

    Initial project setup with NestJS, Prisma, and Swagger. Added configuration files, environment variables, and basic application structure including controllers, services, and DTOs. Integrated PostgreSQL database connection and set up Swagger for API documentation.

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Налаштування Swagger
+  const config = new DocumentBuilder()
+    .setTitle('SSTW Test Task API')
+    .setDescription('API документація для тестового завдання')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    `🚀 Application is running on: http://localhost:${process.env.PORT ?? 3000}`,
+  );
+  console.log(
+    `📚 Swagger documentation: http://localhost:${process.env.PORT ?? 3000}/api`,
+  );
+}
+
+void bootstrap();
